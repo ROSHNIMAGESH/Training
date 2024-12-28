8edb26969abbb708240c2b6446ac7e270ca0d1ed
ROSHNIMAGESH <roshnimagesh2004@gmail.com>
2024-12-28 10:28:33 +0530

    secondayDatatypes, ControlStatement, Looping

---
+++ JavaScript/controlStatements.js
@@ -0,0 +1,8 @@
+a=10
+if(a%2==0)
+{
+    console.log(a,"is Even");
+}
+else{
+    console.log(a,"is Odd");
+}
---
+++ JavaScript/secondaryDataTypes.js
@@ -0,0 +1,28 @@
+//array
+a=[10,20,30,[40.4,50.5],"a","KEC",true]
+console.log(a);
+
+//object type1
+a={
+    "firstname":"Roshni",
+    "lastname":"Magesh",
+    "age":20
+}
+console.log(a);
+
+//object type2
+a={}
+a["firstname"]="Roshni"
+a["lastname"]="Magesh"
+a["age"]=20
+console.log(a);
+console.log(a["firstname"]);
+console.log(a.lastname);
+
+//object type3
+a=new Object()
+a["firstname"]="Roshni"
+a["lastname"]="Magesh"
+a["age"]=20
+console.log(a);
+console.log(a.age);
