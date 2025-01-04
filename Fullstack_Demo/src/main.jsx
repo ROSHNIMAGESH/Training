e2c5a02d73cef430d157703a71f23638394b4ae6
ROSHNIMAGESH <roshnimagesh2004@gmail.com>
2025-01-04 08:56:42 +0530

    main modified

--- Fullstack_Demo/src/main.jsx
+++ Fullstack_Demo/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
 
 // Rendering is the process of generating and updating the UI based on the component's code, state, and props. 
